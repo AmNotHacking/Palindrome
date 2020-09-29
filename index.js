f56e77e2cd66aa468cbbb54fb0cde67c4c22c666
AmNotHacking <theelite2k17@gmail.com>
2020-09-29 02:01:42 -0400

    Project Euler palindrome solution

---
+++ index.js
@@ -0,0 +1,22 @@
+var x, y, product, max = 0;
+
+function Palindromic(x) {
+    var pal = parseInt(x.toString().split('').reverse().join(''));
+
+    if (pal === x)
+        return true;
+    else
+        return false;
+}
+    
+for (x = 100; x <= 999; x++) {
+    for (y = x; y <= 999; y++) {
+        product = x * y;
+        if (Palindromic(product)) {
+          if( max < product ) { // this is new
+            max = product;
+            console.log(x + '*' + y + '=' + product);
+          }
+        }
+    }
+}
